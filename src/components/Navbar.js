b7399f49ab8e0a39e0266967f37da90caa193214
Juliana Fontes <juliana_f.oliveira@hotmail.com>
2022-06-08 20:15:30 -0300

    add FAQ e Footer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,52 @@
 import React from "react";
-import { Navbar,
-         Nav,
-         Container,
-         ButtonGroup,
-         Button,
-         DropdownButton } from "react-bootstrap";
+import {
+  Container,
+  Navbar,
+  Nav,
+  ButtonGroup,
+  Button,
+  NavDropdown,
+} from "react-bootstrap";
 import "./Navbar.scss";
-import logo from "../assets/logo.svg"
+import logo from "../assets/logo.svg";
 
-const Navigation = () => (
+const Navigation = ({ handleCreateAcc }) => (
   <Navbar variant="dark" expand="lg">
-  <Container>
-    <Navbar.Brand href="#home">
-    <img
-        src={logo}
-        height="30"
-        className="d-inline-block align-top"
-        alt="Magbank logo"
-      />
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#cartao">Cartão</Nav.Link>
-        <Nav.Link href="#quemsomos">Quem Somos</Nav.Link>
-        <Nav.Link href="#faq">FAQ</Nav.Link>
-            </Nav>
-            <ButtonGroup aria-label="Basic example">
-    <DropdownButton as={ButtonGroup} variant ="outline-light" title="Acessar minha conta">
-         </DropdownButton>
-  <Button variant="outline-light">Abra sua conta</Button>
-      </ButtonGroup>
-          </Navbar.Collapse>
-  </Container>
-</Navbar>
+    <Container>
+      <Navbar.Brand href="#home">
+        <img
+          src={logo}
+          height="30"
+          className="d-inline-block align-top"
+          alt="Magbank logo"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#cartao">Cartão</Nav.Link>
+          <Nav.Link href="#quemsomos">Quem Somos</Nav.Link>
+          <Nav.Link href="#faq">FAQ</Nav.Link>
+        </Nav>
+        <ButtonGroup aria-label="Basic example" className="ms-auto">
+          <Button variant="outline-light">
+            <NavDropdown title="Acessar minha conta" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">
+                Pessoa Física
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.2">
+                Pessoa Jurídica
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Button>
+          <Button variant="outline-light" onClick={handleCreateAcc}>
+            abra sua conta
+          </Button>
+        </ButtonGroup>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
 );
 
 export default Navigation;
